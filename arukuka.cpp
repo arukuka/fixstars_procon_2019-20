0e7d4aaa4d08ec2dc48f36a158322037d5824596
Shingo Ohtsuka <capriciouswalk@gmail.com>
2020-02-10 23:18:42 +0900

    Debug 出力充実

--- arukuka.cpp
+++ arukuka.cpp
@@ -96,6 +96,20 @@ static std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
 	return os;
 }
 
+template <typename T, size_t N>
+static std::ostream& operator<<(std::ostream& os, const std::array<T, N>& vec)
+{
+	os << "[";
+	for (size_t i = 0; i < N; ++i)
+	{
+		if (i)
+			os << ", ";
+		os << vec[i];
+	}
+	os << "]";
+	return os;
+}
+
 void remove_newline(std::string& s)
 {
 	std::string target("\n");
